23b4b379ab9354b2ab9c56a1296b70cf32453c55
kakinumayuta <kakinuma.y.068@ms.saitama-u.ac.jp>
2025-04-25 15:43:22 +0900

    day1folder

---
+++ day1/inttry.jl
@@ -0,0 +1,14 @@
+import FastGaussQuadrature as FGQ
+import LinearAlgebra as LA
+
+# 積分したい関数
+f(x) = x^2
+
+# n点Gauss-Legendreで重みと点を取得
+n = 10
+x, w = FGQ.gausslegendre(n)
+
+# 区間 [-1, 1] での数値積分
+integral = sum(w .* f.(x))
+
+println("∫_{-1}^{1} x^2 dx ≈ ", integral)
